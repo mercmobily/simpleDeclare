f8b6815a2e11cc16035df562a1e022ad4df1aa4c
Tony Mobily <tonymobily@gmail.com>
2013-08-09 08:23:37 +0200

    Finally added license

--- declare.js
+++ declare.js
@@ -1,3 +1,13 @@
+/*
+Copyright (C) 2013 Tony Mobily
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 var singleDeclare = function(SuperCtor, protoMixin) {
 
   if( typeof( protoMixin ) === 'undefined' ) protoMixin = {};
@@ -59,6 +69,25 @@ var singleDeclare = function(SuperCtor, protoMixin) {
         throw( new Error("Method " + name + "() not inherited!") );
       }
   }
+
+  
+  // PROVISIONAL, to be tested and documented
+  protoMixin.inheritedAsync = function(args, cb ){
+    var name, fn;
+    var argsMinusCallback;
+
+      fn = args.callee.super;
+      if( fn ){
+        argsMinusCallback = Array.prototype.slice.call(args, 0, -1 ).concat( cb )
+
+        return fn.apply( this, argsMinusCallback );
+      } else {
+        throw( new Error("Method " + name + "() not inherited!") );
+      }
+  }
+
+
+
 
   // Copy every element in protoMixin into the prototype.
   for( var k in protoMixin ){
